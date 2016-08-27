bbc8b87a5d95b234121beef7e1e872acf443fc77
zmzhydron <zmzhydron@outlook.com>
2016-08-27 22:55:02 +0800

    dfg

--- localtest/localtest.js
+++ localtest/localtest.js
@@ -5,11 +5,31 @@ function hehe(fn){
 		console.log(this.action+name)
 	} 
 }
+let bba = {
+	gls: function(){
+		console.log('benz gls class suv~~')
+	}
+}
+function addDesc(target, name, desc){
+	var old = desc.value;
+	console.log(old);
+	desc.value = function(...arg){
+		console.log(" function name : ",name);
+		return old.apply(this,arg);
+	}
+}
 @hehe
 class Me{
 	constructor(){
 		this.action = 'fuck you : ';
 	}
+	@addDesc
+	add(a,b){
+		return a+b;
+	}
 }
+Object.assign(Me.prototype,bba);
 var m = new Me();
-m.doyou('zmz');
+m.gls();
+m.doyou("zmz");
+console.log(m.add(1,1));
