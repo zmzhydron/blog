5038e6a8cb00c7417e2890e02974948697f148db
zmzhydron <zmzhydron@outlook.com>
2016-08-27 14:22:59 +0800

    decorator

--- localtest/localtest.js
+++ localtest/localtest.js
@@ -1,18 +1,15 @@
-<<<<<<< HEAD
 'use strict'
-let regexpTest = function(){
-	var a = '123453453425234.23894723874';
 
-	var _a = /(?=(?!\b)(\d{3})+\D|$(?!\b))/g;
-	console.log(a.replace(_a,"@"));
-	console.log(JSON.stringify.toString())
+function hehe(fn){
+	fn.prototype.doyou = function(name){
+		console.log(this.action+name)
+	} 
 }
-regexpTest();
-
-var path = require('path');
-
-// console.log(path.resolve(''));
-
-=======
-11
->>>>>>> 118e7088080981d7cd76f05fbd77d74e2dce9dea
+@hehe
+class Me{
+	constructor(){
+		this.action = 'fuck you : ';
+	}
+}
+var m = new Me();
+m.doyou('zmz');
